3a147512c6da87830acc559f12ec5ff3039befb6
chavalk <chavalk@hotmail.com>
2024-04-06 14:54:40 -0500

    First commit.

---
+++ ThreadsTutorial/ThreadsTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsTutorialApp.swift
+//  ThreadsTutorial
+//
+//  Created by Jose Garcia on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
