a25d5879708991be08cdbab7e9c18ec71af0bc67
chavalk <chavalk@hotmail.com>
2024-04-06 15:02:41 -0500

    Add login view.

---
+++ ThreadsTutorial/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  ThreadsTutorial
+//
+//  Created by Jose Garcia on 4/6/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
