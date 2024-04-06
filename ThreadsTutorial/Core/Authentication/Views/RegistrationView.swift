df12a729a222a786a296c2d7e088d306ef5dfd77
chavalk <chavalk@hotmail.com>
2024-04-06 16:48:24 -0500

    Add registration view.

---
+++ ThreadsTutorial/Core/Authentication/Views/RegistrationView.swift
@@ -0,0 +1,92 @@
+//
+//  RegistrationView.swift
+//  ThreadsTutorial
+//
+//  Created by Jose Garcia on 4/6/24.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var fullName = ""
+    @State private var username = ""
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            Image("threads-app-icon")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 120, height: 120)
+                .padding()
+            
+            VStack {
+                TextField("Enter your email", text: $email)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding(.horizontal, 24)
+                
+                SecureField("Enter your password", text: $password)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding(.horizontal, 24)
+                
+                TextField("Enter your full name", text: $fullName)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding(.horizontal, 24)
+                
+                TextField("Enter your username", text: $username)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding(.horizontal, 24)
+            }
+            
+            Button(action: {
+                
+            }, label: {
+                Text("Sign Up")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .frame(width: 352, height: 44)
+                    .background(.black)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+            })
+            .padding(.vertical)
+            
+            Spacer()
+            
+            Divider()
+            
+            Button(action: {
+                
+            }, label: {
+                HStack(spacing: 3) {
+                    Text("Already have an account?")
+                    
+                    Text("Sign In")
+                        .fontWeight(.semibold)
+                }
+                .foregroundStyle(.black)
+                .font(.footnote)
+            })
+            .padding(.vertical, 16)
+        }
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
