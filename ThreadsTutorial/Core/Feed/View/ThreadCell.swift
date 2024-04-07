af63d472615a6232941a4ce0cd6fbf18eab0b3f7
chavalk <chavalk@hotmail.com>
2024-04-06 21:07:40 -0500

    Finish feed view and add thread cell.

---
+++ ThreadsTutorial/Core/Feed/View/ThreadCell.swift
@@ -0,0 +1,82 @@
+//
+//  ThreadCell.swift
+//  ThreadsTutorial
+//
+//  Created by Jose Garcia on 4/6/24.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top, spacing: 12) {
+                Image("max-verstappen")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack {
+                        Text("maxverstappen1")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                        Spacer()
+                        
+                        Text("10m")
+                            .font(.caption)
+                            .foregroundStyle(Color(.systemGray3))
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            Image(systemName: "ellipsis")
+                                .foregroundStyle(Color(.darkGray))
+                        })
+                    }
+                    
+                    Text("Formula 1 champion")
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                    
+                    HStack(spacing: 16) {
+                        Button(action: {
+                            
+                        }, label: {
+                            Image(systemName: "heart")
+                        })
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            Image(systemName: "bubble.right")
+                        })
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            Image(systemName: "arrow.rectanglepath")
+                        })
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            Image(systemName: "paperplane")
+                        })
+                    }
+                    .foregroundStyle(.black)
+                    .padding(.vertical, 8)
+                }
+            }
+            
+            Divider()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ThreadCell()
+}
