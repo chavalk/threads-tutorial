8a7a6ad745b19032fdff50b67805b606f36fb64e
chavalk <chavalk@hotmail.com>
2024-04-08 18:13:23 -0500

    Add current profile view and refactor app to use profile view for search tab navigation and current profile view for profile tab.

--- ThreadsTutorial/Core/Profile/View/ProfileView.swift
+++ ThreadsTutorial/Core/Profile/View/ProfileView.swift
@@ -8,7 +8,8 @@
 import SwiftUI
 
 struct ProfileView: View {
-    @StateObject var viewModel = ProfileViewModel()
+    let user: User
+    
     @State private var selectedFilter: ProfileThreadFilter = .threads
     @Namespace var animation
     
@@ -17,105 +18,99 @@ struct ProfileView: View {
         return UIScreen.main.bounds.width / count - 16
     }
     
-    private var currentUser: User? {
-        return viewModel.currentUser
-    }
-    
     var body: some View {
-        NavigationStack {
-            ScrollView(showsIndicators: false) {
-                // Bio and stats
-                VStack(spacing: 20) {
-                    HStack(alignment: .top) {
-                        VStack(alignment: .leading, spacing: 12) {
-                            // Full name and username
-                            VStack(alignment: .leading, spacing: 4) {
-                                Text(currentUser?.fullName ?? "")
-                                    .font(.title2)
-                                    .fontWeight(.semibold)
-                                
-                                Text(currentUser?.username ?? "")
+        ScrollView(showsIndicators: false) {
+            // Bio and stats
+            VStack(spacing: 20) {
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading, spacing: 12) {
+                        // Full name and username
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text(user.fullName)
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                            
+                            Text(user.username)
+                                .font(.subheadline)
+                        }
+                        
+                        if let bio = user.bio {
+                            Text(bio)
+                                .font(.footnote)
+                        }
+                        
+                        Text("2 followers")
+                            .font(.caption)
+                            .foregroundStyle(.gray)
+                    }
+                    
+                    Spacer()
+                    
+                    CircularProfileImageView()
+                }
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Follow")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .frame(width: 352, height: 32)
+                        .background(.black)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                })
+                
+                // User content list view
+                VStack {
+                    HStack {
+                        ForEach(ProfileThreadFilter.allCases) { filter in
+                            VStack {
+                                Text(filter.title)
                                     .font(.subheadline)
-                            }
-                            
-                            if let bio = currentUser?.bio {
-                                Text(bio)
-                                    .font(.footnote)
-                            }
-                            
-                            Text("2 followers")
-                                .font(.caption)
-                                .foregroundStyle(.gray)
-                        }
-                        
-                        Spacer()
-                        
-                        CircularProfileImageView()
-                    }
-                    
-                    Button(action: {
-                        
-                    }, label: {
-                        Text("Follow")
-                            .font(.subheadline)
-                            .fontWeight(.semibold)
-                            .foregroundStyle(.white)
-                            .frame(width: 352, height: 32)
-                            .background(.black)
-                            .clipShape(RoundedRectangle(cornerRadius: 8))
-                    })
-                    
-                    // User content list view
-                    VStack {
-                        HStack {
-                            ForEach(ProfileThreadFilter.allCases) { filter in
-                                VStack {
-                                    Text(filter.title)
-                                        .font(.subheadline)
-                                        .fontWeight(selectedFilter == filter ? .semibold : .regular)
-                                    
-                                    if selectedFilter == filter {
-                                        Rectangle()
-                                            .foregroundStyle(.black)
-                                            .frame(width: filterBarWidth, height: 1)
-                                            .matchedGeometryEffect(id: "item", in: animation)
-                                    } else {
-                                        Rectangle()
-                                            .foregroundStyle(.clear)
-                                            .frame(width: filterBarWidth, height: 1)
-                                    }
+                                    .fontWeight(selectedFilter == filter ? .semibold : .regular)
+                                
+                                if selectedFilter == filter {
+                                    Rectangle()
+                                        .foregroundStyle(.black)
+                                        .frame(width: filterBarWidth, height: 1)
+                                        .matchedGeometryEffect(id: "item", in: animation)
+                                } else {
+                                    Rectangle()
+                                        .foregroundStyle(.clear)
+                                        .frame(width: filterBarWidth, height: 1)
                                 }
-                                .onTapGesture {
-                                    withAnimation(.spring()) {
-                                        selectedFilter = filter
-                                    }
+                            }
+                            .onTapGesture {
+                                withAnimation(.spring()) {
+                                    selectedFilter = filter
                                 }
                             }
                         }
-                        
-                        LazyVStack {
-                            ForEach(0 ... 10, id: \.self) { thread in
-                                ThreadCell()
-                            }
+                    }
+                    
+                    LazyVStack {
+                        ForEach(0 ... 10, id: \.self) { thread in
+                            ThreadCell()
                         }
                     }
-                    .padding(.vertical, 8)
                 }
+                .padding(.vertical, 8)
             }
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button(action: {
-                        AuthService.shared.signOut()
-                    }, label: {
-                        Image(systemName: "line.3.horizontal")
-                    })
-                }
-            }
-            .padding(.horizontal)
         }
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button(action: {
+                    AuthService.shared.signOut()
+                }, label: {
+                    Image(systemName: "line.3.horizontal")
+                })
+            }
+        }
+        .padding(.horizontal)
     }
 }
 
 #Preview {
-    ProfileView()
+    ProfileView(user: DeveloperPreview.shared.user)
 }
