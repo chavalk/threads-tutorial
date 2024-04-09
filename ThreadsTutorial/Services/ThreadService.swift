e2c939aad5b08b1ab0c4d7f5eff47b92a385401c
chavalk <chavalk@hotmail.com>
2024-04-08 23:35:10 -0500

    Add thread service, thread, and create thread view model to implement logic to post thread to database.

---
+++ ThreadsTutorial/Services/ThreadService.swift
@@ -0,0 +1,18 @@
+//
+//  ThreadService.swift
+//  ThreadsTutorial
+//
+//  Created by Jose Garcia on 4/8/24.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+struct ThreadService {
+    
+    static func uploadThread(_ thread: Thread) async throws {
+        guard let threadData = try? Firestore.Encoder().encode(thread) else { return }
+        try await Firestore.firestore().collection("threads").addDocument(data: threadData)
+    }
+}
