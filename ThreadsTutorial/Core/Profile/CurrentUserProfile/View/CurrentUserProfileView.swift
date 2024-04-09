f60669819c3e6c3e12a1f6dce2bec2b917526457
chavalk <chavalk@hotmail.com>
2024-04-09 01:20:47 -0500

    Add user content list view model to facilitate fetching user threads.

--- ThreadsTutorial/Core/Profile/CurrentUserProfile/View/CurrentUserProfileView.swift
+++ ThreadsTutorial/Core/Profile/CurrentUserProfile/View/CurrentUserProfileView.swift
@@ -39,7 +39,9 @@ struct CurrentUserProfileView: View {
                     })
                     
                     // User content list view
-                    UserContentListView()
+                    if let user = currentUser {
+                        UserContentListView(user: user)
+                    }
                 }
             }
             .sheet(isPresented: $showEditProfile, content: {
--- ThreadsTutorial/Core/Profile/UserProfile/ProfileView.swift
+++ ThreadsTutorial/Core/Profile/UserProfile/ProfileView.swift
@@ -29,7 +29,7 @@ struct ProfileView: View {
                 })
                 
                 // User content list view
-                UserContentListView()
+                UserContentListView(user: user)
             }
         }
         .navigationBarTitleDisplayMode(.inline)
